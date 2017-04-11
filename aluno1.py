a6de430706ca3c8e58343f0fec8f75f6077257f7
pedrodias98 <pedrovillas@hotmail.com.br>
2017-04-11 15:07:39 -0300

    Versão completa

--- aluno1.py
+++ aluno1.py
@@ -1,14 +1,41 @@
 #ABERTURA DE PROGRA\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 import json
-from Fase_batalha import calcular_batalha
 
 def mostra_ipmon(ipmon):
     print("Inspermon : {0}".format(ipmon["nome"]))
     print("poder = {0}".format(ipmon["poder"]))
     print("vida = {0}".format(ipmon["vida"]))
     print("defesa = {0}\n".format(ipmon["defesa"]))
-
-
+    
+#Fase de Batalha
+    
+def calcular_batalha(selvagem,ipmon2):
+    for ipmon2 in Insperdex:
+        while selvagem["vida"] and ipmon2["vida"] > 0:
+            r = 1
+           
+            if r%2 != 0:
+                
+                selvagem["vida"] = selvagem["vida"] - (ipmon2["poder"] - selvagem["defesa"])
+           
+            elif r%2 == 0:
+                
+                ipmon2["vida"] = ipmon2["vida"] - (selvagem["poder"] - ipmon2["defesa"])
+            print ("Round {0}".format(r))
+            r = r + 1
+           
+            if ipmon2["vida"] <= 0:
+                print("{0} ganhou!!! Você perdeu essa batalha".format(selvagem["nome"]))
+                break
+            else:
+                print("{0} ganhou!!! Você ganhou essa batalha".format(ipmon2["nome"]))
+                if selvagem not in Insperdex:
+                    Insperdex.append(selvagem)
+                    print(Insperdex)
+                break
+            return calcular_batalha(selvagem,ipmon2)
+        
+#Fase 1
 with open('inspermon_world.json') as arquivo:
     inspermons = json.load(arquivo)
 
