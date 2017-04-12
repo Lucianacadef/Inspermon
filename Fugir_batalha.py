4ada531fdbeb22d25f612767cd57f4fb908ec9ef
pedrodias98 <pedrovillas@hotmail.com.br>
2017-04-12 09:45:24 -0300

    mlhoria

--- Fugir_batalha.py
+++ Fugir_batalha.py
@@ -1,11 +1,14 @@
-from random import randint
-numero = randint(1,100)
-fuga = list(range(1,100,2))
-print(fuga)
+
 def fugir_batalha():
-    Fugir = input("Você quer fugir da batalha? Sim ou Não:").lower().title()
-    if Fugir == "Sim":
-        if numero in fuga :
-            print("{0} ganhou!!! Você perdeu essa batalha".format(selvagem["nome"]))
-            return
+    from random import randint
+    numero = randint(1,100)
+    fuga = list(range(1,100,2))
+    print("Seu número foi {0}".format(numero)
+    if numero in fuga:
+        print("Você consiguiu fugir, a sorte está com você")
+        print("{0} ganhou!!! Você perdeu essa batalha".format(selvagem["nome"]))
+        return
+    else:
+        print("A sorte não está com você!!!")
+        
     
