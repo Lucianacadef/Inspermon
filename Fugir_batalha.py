c7e405815f1c8c2e12e020b97123df1ea1037251
pedrodias98 <pedrovillas@hotmail.com.br>
2017-04-12 13:10:10 -0300

    Erros

--- Fugir_batalha.py
+++ Fugir_batalha.py
@@ -3,7 +3,7 @@ def fugir_batalha():
     from random import randint
     numero = randint(1,100)
     fuga = list(range(1,100,2))
-    print("Seu número foi {0}".format(numero)
+    print("Seu número foi {0}".format(numero))
     if numero in fuga:
         print("Você consiguiu fugir, a sorte está com você")
         print("{0} ganhou!!! Você perdeu essa batalha".format(selvagem["nome"]))
